017d317121dd51d533327bba74d955c89630b773
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-26 14:26:42 +0800

    Update RegularExpressionAttribute.cs

--- ValidationComponent/CustomAttributes/RegularExpressionAttribute.cs
+++ ValidationComponent/CustomAttributes/RegularExpressionAttribute.cs
@@ -6,6 +6,7 @@ namespace ValidationComponent.CustomAttributes
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter
     | AttributeTargets.Property, AllowMultiple = false)]
+    //是否允许多次应用于同一个目标 在这种情况下 表示多次应用于同一个目标
     public class RegularExpressionAttribute:Attribute
     {
         public string ErrorMessage { get; set; }
@@ -18,6 +19,7 @@ namespace ValidationComponent.CustomAttributes
             ErrorMessage = "Field, {0}, is invalid. The value provided does not match the declared regular expression pattern, {1}";
         }
         public RegularExpressionAttribute(string pattern, string errorMessage)
+        //pattern 正则表达式模式 和errorMessage 将这些消息都放入方法中
         {
             Pattern = pattern;
             ErrorMessage = errorMessage;
