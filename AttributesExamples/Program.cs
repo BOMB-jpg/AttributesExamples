dbb3e827ef6491d6f06b17be9ed78a5b9b2e3e8a
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-26 14:04:52 +0800

    Update Program.cs

--- AttributesExamples/Program.cs
+++ AttributesExamples/Program.cs
@@ -60,7 +60,8 @@ namespace AttributesExamples
 
 
         }
-
+//输入类型  并判断输入的有效性
+        // 输入的类型 t  提示输入的字符串    和 正在输入字段的属性和名字   作为输出苍蝇 用于存储用户的存储的单元
         private static bool GetInput(Type t, string promptText, string fieldName, out string fieldValue) 
         {
             fieldValue = "";
@@ -69,10 +70,12 @@ namespace AttributesExamples
             do
             {
                 Console.WriteLine(promptText);
+                //提示要输入的字段
                 
                 enteredValue = Console.ReadLine();
-
+                 //输入的字段
                 if (!Validation.PropertyValueIsValid(t, enteredValue, fieldName, out errorMessage))
+                    //验证用户输入的值是否有效。如果验证失败，则显示错误消息，并继续提示用户输入。  没有就是对
                 {
                     fieldValue = null;
                     Console.BackgroundColor = ConsoleColor.Red;
@@ -83,7 +86,7 @@ namespace AttributesExamples
                 }
                 else 
                 {
-                    fieldValue = enteredValue;
+                    fieldValue = enteredValue;  //用于存储值的变量
                     break;
                 }
 
