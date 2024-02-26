aaedd055938c010af1e621af12cf82dfa74300b6
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-26 13:54:03 +0800

    Update Employee.cs

--- AttributesExamples/Models/Employee.cs
+++ AttributesExamples/Models/Employee.cs
@@ -11,10 +11,11 @@ namespace AttributesExamples.Models
 {
     public class Employee
     {
-        [Required]
+        [Required]  // 这个是与通用模型中必须使用的一项注释
         public int Id { get; set; }
         
-        [Required]
+        [Required] 
+        //意思是该属性长度不能超过16并给出了错误信息的模版
         [StringLength(15,"Field, {0}, cannot exceed {1} characters in length and cannot be less than {2} characters in length",2)]
         public string FirstName { get; set; }
         
@@ -26,19 +27,21 @@ namespace AttributesExamples.Models
         [Required]
         [StringLength(15, "Field, {0}, cannot exceed {1} characters in length and cannot be less than {2} characters in length", 2)]
         [RegularExpression(@"\s*\(?0\d{4}\)?\s*\d{6}\s*)|(\s*\(?0\d{3}\)?\s*\d{3}\s*\d{4}\s*")]
+        //用于验证字符串符合给正则表达式 区号为4位，电话号为6位
         [JsonIgnore]
         public string PhoneNumber { get; set; }
         
         [Required]
         [StringLength(15, "Field, {0}, cannot exceed {1} characters in length and cannot be less than {2} characters in length", 2)]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
+      //用于验证电子邮件地址格式的正则表达式
         [JsonIgnore]
         public string EmailAddress { get; set; }
        
         [Required]
         [StringLength(15, "Field, {0}, cannot exceed {1} characters in length and cannot be less than {2} characters in length", 2)]
         [RegularExpression(@"^ ?(([BEGLMNSWbeglmnsw][0-9][0-9]?)|(([A-PR-UWYZa-pr-uwyz][A-HK-Ya-hk-y][0-9][0-9]?)|(([ENWenw][0-9][A-HJKSTUWa-hjkstuw])|([ENWenw][A-HK-Ya-hk-y][0-9][ABEHMNPRVWXYabehmnprvwxy])))) ?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$")]
-       // [JsonIgnore]
+        [JsonIgnore] //它指示序列化器在将对象转换为 JSON 字符串时忽略标记了该注解的属性。
         public string PostCode { get; set; }
     }
 }
