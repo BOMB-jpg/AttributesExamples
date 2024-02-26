7b37555af879c2efc65776bd4d43621e8ba9930b
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-26 14:18:37 +0800

    Update Logging.cs

--- LoggingComponent/Logging.cs
+++ LoggingComponent/Logging.cs
@@ -8,11 +8,11 @@ namespace LoggingComponent
     {
         // [Conditional("LOG_INFO")]
         [Obsolete("The LogToScreen method has now been deprecated. Please use the 'LogToFile' method instead",false)]
-        public static void LogToScreen(string msg)
+        public static void LogToScreen(string msg)  //将日志达到屏幕上日志消息输出到控制台。
         {
             Console.WriteLine(msg);
         }
-        public static void LogToFile(string msg)
+        public static void LogToFile(string msg)   //将日志记录到文件上
         {
             Console.WriteLine("Simulating logging text to file, " + msg);
         }
